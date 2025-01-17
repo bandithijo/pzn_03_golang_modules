6a97dfa15c3db3be3d83d7f10e6de84c570393c4
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-18 07:14:19 +0800

    membuat module

---
+++ 01_membuat_module.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+Membuat Module
+- Untuk membuat module baru, kita bisa menggunakan perintah: go mod init nama-module
+- Go akan secara otomatis membuat file go.mod yang berisikan informasi nama module dan juga versi Go yang kita gunakan
+
+Rilis Module
+- Go terintegrasi baik dengan Git
+- Untuk merilis module, kita hanya perlu membuat Tag di Git
+*/
