7e690a4f7bfbb321496100836621382236224bda
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-19 03:35:55 +0800

    major upgrade

---
+++ 05_major_upgrade.go
@@ -0,0 +1,8 @@
+package main
+
+/*
+Major Upgrade
+- Major upgrade biasanya terjadi dikarenakan ada perubahan pada isi kode program kita, sehingga membuatnya tidak backward compatible
+- Sebaiknya hal ini sebisa mungkin dihindari
+- Namun jika tidak bisa dihindari, strategi terbaik adalah merubah nama module
+*/
