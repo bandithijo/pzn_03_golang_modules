e5670e6653a7a24456d57255630822f673f85be2
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-19 02:58:03 +0800

    upgrade dependency

---
+++ 04_upgrade_dependency.go
@@ -0,0 +1,7 @@
+package main
+
+/*
+Upgrade Dependency
+- Untuk upgrade dependency ke versi terbaru, kita bisa mengubah isi go.mod, lalu mengubah tag nya menjadi tag terbaru
+- Untuk mendownload versi terbaru, gunakan perintah: go get
+*/
